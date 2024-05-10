5d4a9a357fb6219320d6ed8b6322e90244f47738
gyfeuhijds <js1honnelet@hotmail.com>
2024-05-11 03:10:19 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-function findKthLargest(nums, k) {
-  nums.sort((a, b) => b - a);
-  return nums[k - 1];
+function rightSideView(root) {
+  if (!root) return [];
+  const result = [];
+  let level = 0;
+  const traverse = (node, level) => {
+    if (!node) return;
+    if (result[level] === undefined) result[level] = node.val;
+    traverse(node.right, level + 1);
+    traverse(node.left, level + 1);
+  };
+  traverse(root, level);
+  return result;
 }
