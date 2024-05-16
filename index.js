93e4f4bf6378d873e7b78057b2afa583adc8ce36
gyfeuhijds <js1honnelet@hotmail.com>
2024-05-16 14:18:24 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-function averageArray(arr) {
-  return arr.reduce((acc, curr) => acc + curr, 0) / arr.length;
+function calculate(s) {
+  const stack = [];
+  let num = 0;
+  let sign = "+";
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    if (!isNaN(parseInt(char)) && char !== " ") {
+      num = num * 10 + parseInt(char);
+    }
+    if (isNaN(parseInt(char)) || i === s.length - 1) {
+      if (sign === "+") stack.push(num);
+      else if (sign === "-") stack.push(-num);
+      else if (sign === "*") stack.push(stack.pop() * num);
+      else if (sign === "/") stack.push(parseInt(stack.pop() / num));
+      num = 0;
+      sign = char;
+    }
+  }
+  return stack.reduce((acc, val) => acc + val, 0);
 }
