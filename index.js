57aad7d617d45c10ab9ec4ed36ad7c1b3ee88274
gyfeuhijds <js1honnelet@hotmail.com>
2024-05-18 03:34:26 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-function calculate(s) {
-  const stack = [];
-  let num = 0;
-  let sign = "+";
-  for (let i = 0; i < s.length; i++) {
-    const char = s[i];
-    if (!isNaN(parseInt(char)) && char !== " ") {
-      num = num * 10 + parseInt(char);
-    }
-    if (isNaN(parseInt(char)) || i === s.length - 1) {
-      if (sign === "+") stack.push(num);
-      else if (sign === "-") stack.push(-num);
-      else if (sign === "*") stack.push(stack.pop() * num);
-      else if (sign === "/") stack.push(parseInt(stack.pop() / num));
-      num = 0;
-      sign = char;
-    }
-  }
-  return stack.reduce((acc, val) => acc + val, 0);
+function isPalindrome(str) {
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
 }
+
+console.log(isPalindrome("racecar")); // true
